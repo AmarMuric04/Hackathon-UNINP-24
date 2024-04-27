406e1e9c954b59ed11ade29ad652a0526cb241e1
aminnicevic04 <aminnicevic@gmail.com>
2024-04-27 09:49:13 +0200

    whole landing page done by me (my task)

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,44 @@
-import raketa from "../assets/raketa.jpg";
+import { Hero } from "@/Pages/Hero";
+import { GoogleGeminiEffectDemo } from "../Components/ui/Gemini";
+import { ThreeDCardDemo } from "@/Components/ui/ThreeDcardDemo";
+import slika1 from "../public/Assets/jonas-bg-remove.png";
+import slika2 from "../public/Assets/kiril.png";
+import slika3 from "../public/Assets/max.png";
+
+import { MacbookScrollDemo } from "../Components/ui/MacbookScrollDemo";
+import MyFooter from "@/Components/ui/MyFooter";
+
 export default function Home() {
   return (
-  //   <div className="w-full mx-auto">
-  //   <div className="md:my-8 py-0 flex flex-col md:flex-row-reverse items-center justify-between gap-12 ">
-  //           <div className="">
-  //             <img
-  //               src={raketa}
-  //               alt=""
-  //               className="max-sm:h-80 max-sm:w-full max-sm:overflow-auto"
-  //             ></img>
-  //           </div>
-
-  //           <div className="md:w-1/2 lg:ml-20 ml-0 max-lg:text-center">
-  //             <h1
-  //               initial="hidden"
-  //               animate={inView ? "visible" : "hidden"}
-  //               variants={animation}
-  //               transition={{ duration: 0.6, delay: 0.3 }}
-  //               className="text-7xl font-semibold mb-4 text-neutralSilver md:w-4/4 leading-snug max-[700px]:text-4xl"
-  //             >
-  //               Vaše digitalno putovanje{" "}
-  //               <span className="text-brandPrimary leading-snug block">
-  //                 počinje ovde.
-  //               </span>
-  //             </h1>
-  //             <p
-  //               initial="hidden"
-  //               animate={inView ? "visible" : "hidden"}
-  //               variants={animation}
-  //               transition={{ duration: 0.6, delay: 0.4 }}
-  //               className="text-neutralSilver text-2xl mb-8 pt-8 max-md:text-lg"
-  //             >
-  //               Da li ste spremni za uzlet svog brenda i na ostvarivanje porasta
-  //               u prodaji?
-  //             </p>
-             
-  //           </div>
-  // </div>
+    <div className="bg-[#02010a]">
+      <Hero />
+      <GoogleGeminiEffectDemo />
+      <div className="flex width-full justify-center gap-10 bg-[#02010a] ">
+        <ThreeDCardDemo
+          imeProfesora="Jonas Schmeddtman"
+          slika={slika1}
+          deskripcija="Jonass Schmeddtmann je poznati course creator, proslavio se kursem za
+          JavaScript."
+          brojKurseva={11}
+          twitter="jonasschmedtman"
+        />
+        <ThreeDCardDemo
+          imeProfesora="Maximilian Schwarzmüller"
+          slika={slika3}
+          deskripcija="Maximilian Schwarzmüller je nemacki course creator poznat po svojim kursevima za React i Typescript."
+          brojKurseva={7}
+          twitter="maxedapps"
+        />
+        <ThreeDCardDemo
+          imeProfesora="Kirill Eremenko"
+          slika={slika2}
+          deskripcija="Kirill Eremenko, poznatiji kao SuperDataScience. Proslavljeni course creator iz Australije."
+          brojKurseva={8}
+          twitter="kirill_eremenko"
+        />
+      </div>
+      <MacbookScrollDemo />
+      <MyFooter />
+    </div>
   );
 }
