4c493f5738affb7035775b20c17768d6fca5b9b0
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-04-28 13:49:56 +0200

    Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
+    "./Componentse/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
